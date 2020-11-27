00958e35f1075be58000db4722b96ad33c7e85a7
Raiyan <themightyeagle1290@gmail.com>
2020-11-26 22:10:26 -0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,16 @@
+
+public class Player {
+  String name;
+  double health;
+
+  public Player(String name, double health) {
+    this.name = name;
+    this.health = health;
+  }
+
+  public attack() {
+    //placeholder value
+    return 10;
+  }
+
+}
